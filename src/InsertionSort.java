ad8867766b39c80c57080c2afc444cd7e5d22e57
lmt6498 <lmt6498@users.noreply.github.com>
2021-06-24 10:54:33 +0700

    Nop bai tap

---
+++ src/InsertionSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+        insertionSortByStep(list);
+    }
+
+    public static void insertionSortByStep(int[] list) {
+        int pos, x;
+        for (int i = 1; i < list.length; i++) {
+            x = list[i];
+            pos = i;
+            while (pos > 0 && x < list[pos - 1]) {
+                System.out.println("Đổi chỗ " + list[pos - 1] + " lên vị trí của " + list[pos]);
+                list[pos] = list[pos - 1];
+                pos--;
+            }
+
+            list[pos] = x;
+            System.out.print("List after the  " + i + "' sort: ");
+            for (int j = 0; j < list.length; j++) {
+                System.out.print(list[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
